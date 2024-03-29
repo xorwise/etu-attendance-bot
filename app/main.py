fb30c3a7cb45ecc72d82a72f3d4acea9c39fb9bf
xorwise <xorwise@gmail.com>
2024-03-30 00:16:49 +0300

    init commit for telegram-bot api

--- app/main.py
+++ app/main.py
@@ -1,16 +1,36 @@
-from fastapi import FastAPI
-import uvicorn
-from fastapi.staticfiles import StaticFiles
-from database.queries import create_table
-from views.views import router
+import asyncio
+import os
+from database import queries
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from handlers.auth import auth_router
+from handlers.account import account_router
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(router)
+load_dotenv(".env")
+TOKEN = os.environ.get("BOT_TOKEN", None)
+
+dp = Dispatcher()
+dp.include_routers(auth_router, account_router)
+
+bot = Bot(TOKEN)
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+    await message.answer(
+        f"Привет, <b>{hbold(message.from_user.full_name)}</b>!",
+        parse_mode=ParseMode.HTML,
+    )
+
+
+async def main() -> None:
+    await queries.create_tables()
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    load_dotenv(".env")
-    create_table()
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    asyncio.run(main())
