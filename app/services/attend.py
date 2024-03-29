a3074430f0f05b892fbf3bbbeaa31fe6a7ff3ba0
xorwise <xorwise@gmail.com>
2024-03-30 00:21:21 +0300

    multiple fixes on attendance check

--- app/services/attend.py
+++ app/services/attend.py
@@ -1,19 +1,35 @@
-from fastapi import HTTPException
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
+from utils.exceptions import EtuAuthException
 
 
-def login(email: str, password: str) -> WebDriver:
+class MyDriver:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def __enter__(self):
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.quit()
+
+
+def create_driver() -> WebDriver:
     options = Options()
-    # options.add_argument("--no-sandbox")
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--disable-dev-shm-usage")
-    # options.add_argument("--headless")
-    # options.add_argument("--start-maximized")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--start-maximized")
 
-    driver = webdriver.Chrome()
+    return webdriver.Chrome()
+
+
+def login(email: str | None, password: str | None) -> WebDriver:
+    driver = create_driver()
     driver.get("https://id.etu.ru/login")
     time.sleep(2)
 
@@ -21,9 +37,11 @@ def login(email: str, password: str) -> WebDriver:
     password_field = driver.find_element(by="name", value="password")
 
     login_button = driver.find_element(by="xpath", value="//button[@type='submit']")
+    checkbox = driver.find_element(by="id", value="remember")
 
     username_field.send_keys(email)
     password_field.send_keys(password)
+    checkbox.click()
 
     login_button.click()
 
@@ -36,10 +54,10 @@ def login(email: str, password: str) -> WebDriver:
         return driver
     else:
         driver.quit()
-        raise HTTPException(status_code=422, detail=text)
+        raise EtuAuthException(text)
 
 
-def login_lk(email: str, password: str, driver: WebDriver) -> WebDriver:
+def login_lk(email: str, password: str, driver: WebDriver) -> list[dict]:
     driver.get("https://lk.etu.ru/login")
     username_field = driver.find_element(by="name", value="email")
     password_field = driver.find_element(by="name", value="password")
@@ -50,26 +68,60 @@ def login_lk(email: str, password: str, driver: WebDriver) -> WebDriver:
 
     login_button.click()
     time.sleep(3)
-    return driver
+    return driver.get_cookies()
 
 
-def attend(driver: WebDriver):
-    driver.get("https://digital.etu.ru/attendance/auth")
-    time.sleep(3)
+def attend(cookies: list[dict]) -> list[str]:
+    # service = Service("/home/xorwise/Coding/etu/app/chromedriver")
 
-    button = driver.find_element(by="class name", value="btn")
-    button.click()
+    my_driver = MyDriver(create_driver())
+    with my_driver as driver:
+        id_cookie = {}
+        lk_cookie = {}
+        for cookie in cookies:
+            if cookie["domain"] in "id.etu.ru":
+                id_cookie = cookie
+            elif cookie["domain"] in "lk.etu.ru":
+                lk_cookie = cookie
+        if not id_cookie or not lk_cookie:
+            raise EtuAuthException("Cookies are invalid")
 
-    time.sleep(3)
-    login_button = driver.find_element(by="xpath", value="//button[@type='submit']")
-    login_button.click()
+        driver.get("https://id.etu.ru/")
+        driver.add_cookie(id_cookie)
 
-    time.sleep(3)
+        driver.get("https://lk.etu.ru/")
+        driver.add_cookie(lk_cookie)
 
-    buttons = driver.find_elements(by="xpath", value="//*[text()=' Отметиться ']")
+        driver.get("https://digital.etu.ru/attendance/auth")
+        time.sleep(2)
 
-    for button in buttons:
+        button = driver.find_element(by="class name", value="btn")
         button.click()
-        time.sleep(1)
 
-    driver.quit()
+        time.sleep(3)
+        login_button = driver.find_element(by="xpath", value="//button[@type='submit']")
+        login_button.click()
+
+        time.sleep(3)
+        if "id.etu.ru" in driver.current_url:
+            raise EtuAuthException("Cookies are invalid")
+
+        # buttons = driver.find_elements(by="xpath", value="//*[text()=' Отметиться ']")
+        rows = driver.find_elements(by="class name", value="card-body")
+        titles = []
+        for row in rows:
+            try:
+                button = row.find_element(
+                    by="xpath", value="//*[text()=' Отметиться ']"
+                )
+                button.click()
+                titles.append(
+                    " ".join(
+                        el.text
+                        for el in row.find_elements(by="class name", value="card-title")
+                    )
+                )
+                time.sleep(1)
+            except Exception:
+                continue
+        return titles
