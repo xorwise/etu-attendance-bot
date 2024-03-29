7904d48ac686ae0e5f6739bf5e0bcd03b4583d41
xorwise <xorwise@gmail.com>
2024-03-30 00:15:23 +0300

    added celery scheduled checks
    feat: celery attendance background checking
    fix: attend function

--- app/worker.py
+++ app/worker.py
@@ -1,7 +1,14 @@
+from aiogram import Bot
 from celery import Celery
 from celery.schedules import crontab
 import os
 from dotenv import load_dotenv
+from database import queries
+from services.attend import attend
+from database.queries import delete_user, get_all_users
+import asyncio
+
+from utils.exceptions import EtuAuthException
 
 load_dotenv(".env")
 
@@ -10,32 +17,47 @@ celery.conf.broker_url = os.getenv("CELERY_BROKER_URL")
 celery.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND")
 
 
+async def attend_users_async():
+    bot = Bot(os.environ.get("BOT_TOKEN", ""))
+    for user in await get_all_users():
+        cookies = await queries.get_cookies_by_user(user.email)
+        try:
+            subjects = attend(cookies)
+            subjects_string = ""
+            for subject in subjects:
+                subjects_string += f"{subject}\n"
+            if len(subjects) > 0:
+                await bot.send_message(
+                    chat_id=user.id,
+                    text=f"Отметились на предметах✅\n{subjects_string} ",
+                )
+        except EtuAuthException:
+            await bot.send_message(
+                chat_id=user.id,
+                text="Сессия личного кабинета истекла❌\nПожалуйста, авторизуйтесь заново с помощью команды /login.",
+            )
+            await delete_user(user.id)
+    await bot.session.close()
+
+
 @celery.task
 def attend_users():
-    from services.attend import attend, login, login_lk
-    from database.queries import get_all_users
-    import time
+    asyncio.run(attend_users_async())
 
-    for user in get_all_users():
-        try:
-            driver = login(user.email, user.password)
-            time.sleep(3)
-            driver = login_lk(user.email, user.password, driver)
-            attend(driver)
-            driver.quit()
-        except Exception as e:
-            print(e)
-            continue
+
+# crontab(
+#     minute=50,
+#     hour="8, 9, 11, 13, 15, 17",
+#     day_of_week="mon,tue,wed,thu,fri,sat",
+# ),
 
 
 celery.autodiscover_tasks()
 celery.conf.beat_schedule = {
     "attend_users": {
-        "task": "attend_users",
+        "task": "worker.attend_users",
         "schedule": crontab(
-            minute=50,
-            hour="8, 9, 11, 13, 15, 17",
-            day_of_week="mon,tue,wed,thu,fri,sat",
+            minute="*/1",
         ),
     }
 }
